df9f86bb7e95e10ce20c5cb58be9bae11d6ece3c
liushmh <leviliushmh@gmail.com>
2015-08-03 01:28:34 +0000

    add private in case being called by other object

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def current_user
-      # @_current_user ||= session[:current_user_id] &&
-      #   User.find_by(id: session[:current_user_id])
-        
-        uid = session[:current_user_id] != nil ? session[:current_user_id] : 1
-        @_current_user = 
-        User.find_by(id: uid)
-  end
-  
-  def list_user
-      @users = User.all
-  end
+  private
+    def current_user
+        # @_current_user ||= session[:current_user_id] &&
+        #   User.find_by(id: session[:current_user_id])
+          
+          uid = session[:current_user_id] != nil ? session[:current_user_id] : 1
+          @_current_user = 
+          User.find_by(id: uid)
+    end
+    
+    def list_user
+        @users = User.all
+    end
 end
